d2f431d1513e55b838bab52e74f2a73931ffab41
UzziD <uzzisoft@gmail.com>
2021-10-24 13:36:14 -0600

    added shutdown command and error handler

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
 import os
 from discord.ext import commands
+from discord.ext.commands.core import is_owner
 
 class Commands(commands.Cog):
     def __init__(self, bot):
@@ -21,17 +22,26 @@ class Commands(commands.Cog):
         except Exception as e:
             await ctx.send("Error opening file.")
             print("Exception occured: {}".format(e))
-
-
-
-        #await ctx.send("you've logged {} {}".format(count, item))
-        #print()
     
     @logspirit.error
-    async def on_command_error(self,ctx,error):
+    async def logspirit_error(self,ctx,error):
         if isinstance(error, commands.BadArgument):
             await ctx.send("Invalid arguments provided. Format: !log <item name> <count of item>")
         else:
             print("error on command: {}".format(ctx.command))
             await ctx.send("There was an error!")
 
+
+    @commands.command(name='shutdown')
+    @is_owner()
+    async def shutdown(self, ctx):
+        print("shutting down")
+        await self.bot.close()
+
+    @shutdown.error
+    async def shutdown_error(self,ctx,error):
+        if isinstance(error, commands.NotOwner):
+            await ctx.send("Only the owner of the bot can shut it down.")
+        else:
+            print("error shutting down: {}".format(error))
+            await ctx.send("Error shutting down, check terminal.")
